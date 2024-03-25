3100585900356d84b2b3a42476928925ba10e2fa
Dmytro Turskyi <dmytro.turskyi@gmail.com>
2024-03-24 22:37:03 -0400

    Refactor CartEntry component to allow dynamic quantity selection
    Introduce dynamic quantity selection feature in CartEntry for better user interaction and updated total price display. Add setProductQuantity function prop for handling quantity updates efficiently.

--- src/app/cart/CartEntry.tsx
+++ src/app/cart/CartEntry.tsx
@@ -1,16 +1,29 @@
 'use client';
 
 import { CartItemWithProduct } from '@/lib/db/cart';
+import { formatPrice } from '@/lib/format';
 import Image from 'next/image';
 import Link from 'next/link';
+import { setProductQuantity } from './actions';
+import { useTransition } from 'react';
 
 interface CartEntryProps {
   cartItem: CartItemWithProduct;
+  setProductQuantity: (productId: string, quantity: number) => Promise<void>;
 }
 
 export default function CartEntry({
   cartItem: { product, quantity },
 }: CartEntryProps) {
+  const [isPending, startTransition] = useTransition();
+  const quantityOptions: JSX.Element[] = [];
+  for (let i = 1; i <= 99; i++) {
+    quantityOptions.push(
+      <option value={i} key={i}>
+        {i}
+      </option>,
+    );
+  }
   return (
     <div>
       <div className='flex flex-wrap items-center gap-3'>
@@ -25,6 +38,28 @@ export default function CartEntry({
           <Link href={'/products/' + product.id} className='font-bold'>
             {product.name}
           </Link>
+          <div>Price: {formatPrice(product.price)}</div>
+          <div className='my-1 flex items-center gap-2'></div>
+          Quantity:
+          <select
+            className='select select-bordered w-full max-w-[80px]'
+            defaultValue={quantity}
+            onChange={(e) => {
+              const newQuantity = parseInt(e.currentTarget.value);
+              startTransition(async () => {
+                await setProductQuantity(product.id, newQuantity);
+              });
+            }}
+          >
+            <option value={0}>0 (Remove)</option>
+            {quantityOptions}
+          </select>
+          <div className='flex items-center gap-3'>
+            Total: {formatPrice(product.price * quantity)}
+          </div>
+          {isPending && (
+            <span className='loading loading-spinner loading-sm'></span>
+          )}
         </div>
       </div>
       <div className='divider' />
